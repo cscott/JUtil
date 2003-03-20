000a2e00773621a727a7ec728ba2e8b7815f2e15
C. Scott Ananian <cscott@cscott.net>
2003-03-20 03:00:51 +0000

    Have the jutil.jar file print the library version number if executed.

--- src/net/cscott/jutil/Version.java
+++
@@ -1,9 +0,0 @@
-// -*-java-*-
-package net.cscott.jutil;
-
-public abstract class Version {
-    public static final String PACKAGE_NAME = "JUtil";
-    public static final String PACKAGE_VERSION = "0.0";
-    public static final String PACKAGE_STRING = "JUtil 0.0";
-    public static final String PACKAGE_BUGREPORT = "cscott@cscott.net";
-}
