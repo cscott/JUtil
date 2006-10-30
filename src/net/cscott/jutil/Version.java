83968fa151a35b1cfe80427c39a2cbfd68b02ac3
C. Scott Ananian <cscott@cscott.net>
2006-10-30 19:12:05 +0000

    Hide my email address from the internet (exposes via javadoc's "constant field values" list).

--- src/net/cscott/jutil/Version.java
+++ src/net/cscott/jutil/Version.java
@@ -2,9 +2,9 @@
 package net.cscott.jutil;
 
 /**
- * The <code>Version</code> object contains fields naming the current version
- * (@PACKAGE_VERSION@) of the @PACKAGE_NAME@ library.
- * @version $Id: Version.java,v 1.3 2006-10-29 16:51:43 cananian Exp $
+ * The {@link Version} object contains fields naming the current version
+ * of the library.
+ * @version $Id: Version.java,v 1.4 2006-10-30 19:12:05 cananian Exp $
  */
 public abstract class Version {
     /** The name of the package: "JUtil". */
@@ -14,7 +14,8 @@ public abstract class Version {
     /** The package name and version as one string. */
     public static final String PACKAGE_STRING=PACKAGE_NAME+" "+PACKAGE_VERSION;
     /** The address to which bug reports should be sent. */
-    public static final String PACKAGE_BUGREPORT = "jutil@cscott.net";
+    public static final String PACKAGE_BUGREPORT =
+        new String("jutil@cscott.net").intern(); // hide from the web.
 
     /** Prints the package version if invoked. */
     public static void main(String[] args) {
