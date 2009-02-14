5cdfe9bab756b73037ea3e48dda7a164ff64ef8a
C. Scott Ananian <cscott@cscott.net>
2009-02-14 18:10:57 -0500

    Update build and upload scripts for git.

--- upload.sh
+++ upload.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# before release, change version here, as well as in build-user.xml and in
-# src/net/cscott/jutil/Version.java
+# Version number is kept in build-user.xml.  Change it there.
 PACKAGE=jutil
-VERSION=1.3
+VERSION=$(ant echo-version | fgrep "Current version is: " | sed -e 's/^.*Current version is: //')
 # build prerequisites
-ant clean build javadoc jar dist
+ant clean version build javadoc jar dist
 # make upload bundle
 /bin/rm -rf ${PACKAGE}-${VERSION}
 mkdir ${PACKAGE}-${VERSION}
@@ -13,10 +12,13 @@ mv ${PACKAGE}-${VERSION}/api ${PACKAGE}-${VERSION}/doc
 (date ; echo "Released $PACKAGE $VERSION" ; echo " " ; cat ChangeLog)\
                 > ${PACKAGE}-${VERSION}/ChangeLog.txt
 cp ${PACKAGE}-${VERSION}.tar.gz ${PACKAGE}-${VERSION}/
+gunzip ${PACKAGE}-${VERSION}/${PACKAGE}-${VERSION}.tar.gz
+gzip --rsyncable ${PACKAGE}-${VERSION}/${PACKAGE}-${VERSION}.tar
 ( cd ${PACKAGE}-${VERSION} && \
     ln -s ${PACKAGE}-${VERSION}.tar.gz ${PACKAGE}.tar.gz )
-cp ${PACKAGE}.jar ${PACKAGE}-${VERSION}/
+cp ${PACKAGE}.jar ${PACKAGE}-${VERSION}/${PACKAGE}-${VERSION}.jar
 touch ${PACKAGE}-${VERSION}/VERSION_${VERSION}
-tar -c ${PACKAGE}-${VERSION}/ | \
-    ssh k2.csail.mit.edu "mkdir -p public_html/Projects/JUtil && cd public_html/Projects/JUtil && /bin/rm -rf ${PACKAGE}-${VERSION} && tar -xv"
+# transfer to the distribution machine.
+rsync -avyz --delete-after --copy-dest=jutil-1.3 --copy-dest=jutil-1.2 ${PACKAGE}-${VERSION} cscott.net:public_html/Projects/JUtil/
+
 /bin/rm -rf ${PACKAGE}-${VERSION}
